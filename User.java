90f8e3fc104720008b0bb85d0a498be28e225374
AH56k <99696128+Armen-Hammer@users.noreply.github.com>
2022-04-06 21:04:24 -0400

    First commit for user object

---
+++ User.java
@@ -0,0 +1,30 @@
+public class User {
+    private String username;
+    private String password;
+
+    User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public boolean equals(String username, String password) {
+        return this.username.equals(username) && this.password == password;
+    }
+    
+}
