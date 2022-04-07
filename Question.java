f5ede3ff7374d635ef03d81e43457f0944756472
AH56k <99696128+Armen-Hammer@users.noreply.github.com>
2022-04-06 21:15:31 -0400

    Initial Question commit

---
+++ Question.java
@@ -0,0 +1,15 @@
+public class Question {
+    private String question;
+    
+    Question(String question) {
+        this.question = question;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+}
