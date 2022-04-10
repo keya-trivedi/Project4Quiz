a8bea03d827f35a1cdb615c9fed853f9530ce705
AH56k <99696128+Armen-Hammer@users.noreply.github.com>
2022-04-10 14:19:47 -0400

    Added the functionality to ask the question and return an answered question

--- TFquestion.java
+++ TFquestion.java
@@ -1,3 +1,6 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class TFquestion extends Question {
     private boolean answer;
 
@@ -6,12 +9,54 @@ public class TFquestion extends Question {
         this.question = question;
     }
 
+    public boolean check(boolean answer) {
+        return this.answer == answer;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         TFquestion that = (TFquestion) o;
         return answer == that.answer && this.question.equals(that.question);
+    }
+
+
+    public AnsweredQuestion getAnswer(Scanner scanner) { //return true if it is the right answer
+        String prompt = question + "\n" + String.format("1.True\n2.False");
+
+        int num = 0;
+        boolean readInt = false;
+
+        do {
+            try {
+                System.out.println(prompt);
+                num = scanner.nextInt();
+
+                if(num <= 2 && num >= 1) {
+                    readInt = true;
+                }
+                else {
+                    System.out.printf("Please enter a valid integer from %d to %d\n", 1, 2);
+                }
+            } catch (InputMismatchException e) {
+                System.out.printf("Please enter a valid integer from %d to %d\n", 1, 2);
+            }
+        } while (!readInt);
+
+
+        boolean right;
+        String ans;
+        if (num == 1) {
+            ans = "true";
+            right =  check(true);
+        } else {
+            ans = "false";
+            right = check(false);
+        }
+
+        return new AnsweredQuestion(this, ans, right);
+
     }
 
 }
