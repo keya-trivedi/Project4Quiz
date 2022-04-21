760f076ddcfe3c0f0516d9c869cee72653f133cb
AH56k <99696128+Armen-Hammer@users.noreply.github.com>
2022-04-21 17:04:11 -0400

    Added functionality for deleting and adding courses

--- TeacherBaseView.java
+++ TeacherBaseView.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class TeacherBaseView extends JFrame implements ActionListener {
 
@@ -11,7 +12,7 @@ public class TeacherBaseView extends JFrame implements ActionListener {
     JLabel prompt = new JLabel("WHAT DO YOU WANT TO DO?");
     JButton makeCourseButton = new JButton("MAKE COURSE");
     JButton editCourseButton = new JButton("EDIT COURSE");
-    JButton deleteCourseButton = new JButton("EDIT COURSE");
+    JButton deleteCourseButton = new JButton("DELETE COURSE");
     JButton logoutButton = new JButton("LOGOUT");
     JButton resetPassButton = new JButton("RESET PASSWORD");
     JButton resetNameButton = new JButton("RESET USERNAME");
@@ -72,32 +73,56 @@ public class TeacherBaseView extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        int x = 0;
-        if (e.getSource() == delAccountButton) {
-            try {
+        try {
+            if (e.getSource() == delAccountButton) {
                 oos.writeObject(Server.DELETE_ACCOUNT);
                 oos.flush();
                 String response = (String) ois.readObject();
-                if(response.equals("Success")) {
+                if (response.equals("Success")) {
                     JOptionPane.showMessageDialog(this, "Success");
                     this.dispose();
                     LoginScreen newFrame = new LoginScreen(socket, pw, oos, ois);
                     Utils.makeFrameFromTemplate(newFrame, "Login");
                 }
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            } catch (ClassNotFoundException ex) {
-                ex.printStackTrace();
-            }
-        } else if (e.getSource() == resetNameButton) {
-            this.dispose();
-            ResetUsernameScreen currentScreen = new ResetUsernameScreen(socket, pw, oos, ois);
-            Utils.makeFrameFromTemplate(currentScreen, "Edit username");
-        } else if (e.getSource() == resetPassButton) {
-            ResetPasswordScreen currentScreen = new ResetPasswordScreen(socket, pw, oos, ois);
-            Utils.makeFrameFromTemplate(currentScreen, "Edit password");
-        } else if (e.getSource() == makeCourseButton) {
+            } else if (e.getSource() == resetNameButton) {
+                this.dispose();
+                ResetUsernameScreen currentScreen = new ResetUsernameScreen(socket, pw, oos, ois);
+                Utils.makeFrameFromTemplate(currentScreen, "Edit username");
+            } else if (e.getSource() == resetPassButton) {
+                ResetPasswordScreen currentScreen = new ResetPasswordScreen(socket, pw, oos, ois);
+                Utils.makeFrameFromTemplate(currentScreen, "Edit password");
+            } else if (e.getSource() == makeCourseButton) {
+                this.dispose();
+                CreateCourseScreen currentScreen = new CreateCourseScreen(socket, pw, oos, ois);
+                Utils.makeFrameFromTemplate(currentScreen, "Create course");
+            } else if (e.getSource() == editCourseButton) {
+                oos.writeObject(Server.GET_COURSES_STR);
+                String response = (String) ois.readObject();
+                if (response.equals("Success")) {
+                    ArrayList<String> arr = (ArrayList<String>) ois.readObject();
+                    this.dispose();
+                    ShowCoursesDropdownScreen currentScreen = new ShowCoursesDropdownScreen(socket, pw, oos, ois, arr, Server.EDIT_COURSE, this);
+                    Utils.makeFrameFromTemplate(currentScreen, "Courses");
+                } else {
+                    JOptionPane.showMessageDialog(this, response);
+                }
+            } else if (e.getSource() == deleteCourseButton) {
+                oos.writeObject(Server.GET_COURSES_STR);
+                String response = (String) ois.readObject();
+                if (response.equals("Success")) {
+                    ArrayList<String> arr = (ArrayList<String>) ois.readObject();
+                    this.setVisible(false);
+                    ShowCoursesDropdownScreen currentScreen = new ShowCoursesDropdownScreen(socket, pw, oos, ois, arr, Server.DELETE_COURSE, this);
+                    Utils.makeFrameFromTemplate(currentScreen, "Courses");
+                } else {
+                    JOptionPane.showMessageDialog(this, response);
+                }
 
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
         }
     }
 
