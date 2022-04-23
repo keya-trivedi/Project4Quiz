29bea767811375bf18cfdab52e0aff1c9b03b67d
AH56k <99696128+Armen-Hammer@users.noreply.github.com>
2022-04-23 18:11:58 -0400

    where to run the client program from

---
+++ Client.java
@@ -0,0 +1,19 @@
+
+import java.io.*;
+import java.net.*;
+
+public class Client {
+
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        Socket socket = new Socket("127.0.0.1",4242);
+        PrintWriter pw = new PrintWriter(socket.getOutputStream());
+        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+
+        LoginScreen currentFrame = new LoginScreen(socket, pw, oos, ois);
+        Utils.makeFrameFromTemplate(currentFrame, "Login");
+
+    }
+}
